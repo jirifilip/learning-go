a6b2f71624a9fc5378f0c5ada8428fdf2dd4272f
Jiří Filip <j.f.ilip@seznam.cz>
2025-11-29 16:54:51 +0100

    Write a test for directory listing

--- internal/utils/lister.go
+++ internal/utils/lister.go
@@ -1,5 +1,26 @@
 package utils
 
+import (
+	"log"
+	"os"
+)
+
 func List() int {
 	return 5
+}
+
+func GetFilenames(path string) []string {
+	entries, err := os.ReadDir(path)
+
+	if err != nil {
+		log.Panic("Could not list directory!")
+	}
+
+	filenames := make([]string, len(entries))
+
+	for i, entry := range entries {
+		filenames[i] = entry.Name()
+	}
+
+	return filenames
 }
--- internal/utils/lister_test.go
+++ internal/utils/lister_test.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"os"
+	"path/filepath"
+	"slices"
 	"testing"
 )
 
@@ -9,5 +12,27 @@ func TestList(t *testing.T) {
 
 	if result != 5 {
 		t.Error("Result not equal to 5.")
+	}
+}
+
+func TestGetFilenames(t *testing.T) {
+	directory, err := os.MkdirTemp("", "testdir")
+
+	if err != nil {
+		t.Fatal("Unable to create temporary directory")
+	}
+
+	testFilenames := []string{"file1", "file2", "file99"}
+
+	for _, filename := range testFilenames {
+		fullpath := filepath.Join(directory, filename)
+
+		os.WriteFile(fullpath, []byte("Hello world!"), 0666)
+	}
+
+	result := GetFilenames(directory)
+
+	if !slices.Equal(result, testFilenames) {
+		t.Fatalf("Files not equal: %v != %v", result, testFilenames)
 	}
 }
