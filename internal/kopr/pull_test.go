54e99e6ba6d161458ecd23997a35f7517fce0e7d
Jiří Filip <j.f.ilip@seznam.cz>
2025-11-30 10:18:03 +0100

    Fix test for pulling images

--- internal/kopr/pull_test.go
+++ internal/kopr/pull_test.go
@@ -6,14 +6,18 @@ import (
 	"testing"
 )
 
+func validateURLParam(key string, expectedValue string, request *http.Request, writer http.ResponseWriter) {
+	actualValue := request.URL.Query().Get(key)
+
+	if actualValue != expectedValue {
+		writer.WriteHeader(http.StatusNotFound)
+	}
+}
+
 func TestGetToken(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		scope := r.PathValue("scope")
-
-		if scope != "repository:ubuntu:pull" {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
+		validateURLParam("scope", "repository:ubuntu:pull", r, w)
+		validateURLParam("service", "registry.docker.io", r, w)
 
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("{\"token\":\"XASDXSADSADSXASXASDASDASDAXXSADASDASXASDASDASXASDASDADSXASDASDASXASDASDASXXSXXASDSAXAXSXF\"}"))
